ea73e8ba96e2bbf112ae0b9ee815e98045fc66a8
Karina <karina.kvych@gmail.com>
2023-08-07 12:19:15 +0100

    Added GitHub link

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
-import { useState } from 'react'
+import React from 'react'
 import './App.css'
 
 export default function App() {
-  const [count, setCount] = useState(0)
 
-  return 
+
+  return (
+    <>
+    <div>
+    <h1>Weather App</h1>
+    <footer>
+      This project was coded by Karina Kvych and is {" "}
+      <a href="https://github.com/karin0chka/w5React_Weather"
+      target='_blank' > open-sourced on GitHub</a>
+      </footer>
+      </div>
+    </>
+  )
 }
 
